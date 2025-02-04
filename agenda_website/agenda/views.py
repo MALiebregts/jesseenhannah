310f864ecaf075c2e69ebecd20df2f4ea9678785
markliebregts <mark.liebregts@gmail.com>
2025-02-04 21:11:15 +0100

    second

--- agenda_website/agenda/views.py
+++ agenda_website/agenda/views.py
@@ -3,4 +3,4 @@ from .models import AgendaItem
 
 def agenda_view(request):
     items = AgendaItem.objects.all()
-    return render(request, 'agenda/agenda.html', {'items': items})
+    return render(request, 'agenda_website/agenda.html', {'items': items})
